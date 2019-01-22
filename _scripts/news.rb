f1f53935a037b8e5e3e15c1d8a143581463b57c9
Joe Wilson <joe@joecode.com>
2019-01-22 12:40:04 -0800

    newsapi integration

--- _scripts/news.rb
+++ _scripts/news.rb
@@ -1,92 +1,67 @@
-require 'digest/sha1'
-require 'httparty'
-require 'htmlentities'
+require 'base64'
 require 'json'
-require 'nokogiri'
-require 'uri'
+require 'news-api'
 require 'yaml'
-
-require_relative './_user_agents'
-require_relative './_get_news'
-require_relative './_get_crossfit_journal'
-require_relative './_get_crossfit_mainsite'
-require_relative './_get_grams'
-require_relative './_get_tierthreetactical'
-require_relative './_get_tnation'
-require_relative './_get_shape'
 
 puts "news.rb"
 
+# Date to process?
 if ARGV[0]
   as_of_date = ARGV[0]
 else
   raise "Please provide a date to process. Example: ruby news.rb 2018-05-31"
 end
 
-# Posts
-posts = []
+# Necessary API credentials?
+raise "Please define your News API key: export NEWSAPI_KEY=\"1234\"" if !ENV['NEWSAPI_KEY']
 
-# Sources
-sources = YAML.load_file('./_data/sources.yml')
-
-# Process sources with an RSS feed
-sources.each do |source|
-  if source['feed'] and source['feed'] != ''
-    news = get_news(source['feed'],source['title'],as_of_date,source['category'])
-    posts.push(*news)
-  end
-end
-
-# Process CrossFit Journal JSON feed
-news = get_crossfit_journal(as_of_date)
-posts.push(*news)
-
-# Process CrossFit Mainsite JSON feed
-news = get_crossfit_mainsite(as_of_date)
-posts.push(*news)
-
-# Process Tier Three Tactical HTML page
-news = get_tierthreetactical(as_of_date)
-posts.push(*news)
-
-# Process T-Nation HTML page
-news = get_tnation(as_of_date)
-posts.push(*news)
-
-# Process Shape HTML page
-news = get_shape(as_of_date)
-posts.push(*news)
-
-# IG Sources
-grams = YAML.load_file('./_data/grams.yml')
-
-# Process IG
-grams.each do |source|
-  news = get_grams(source['ig'],as_of_date)
-  posts.push(*news)
-end
-
-# Men's Fitness?
-# https://www.menshealth.com/fitness/
-
-# https://blog.feedspot.com/fitness_rss_feeds/
-# 
-
-# We Are Ladder?
-# https://weareladder.com/blogs/nmp.atom
-
-# Build Jekyll Front Matter
-front_matter = {
-  'layout' => 'post',
-  'links' => posts.shuffle
-}
-
-# Build a Jekyll page, but only if one doesn't exist.
-# We don't want to override any hand crafted content.
+# Determine if we've already processed this date
 news_filename = '_posts/'+as_of_date+'-Noise.md'
+
 if !File.exist? news_filename
+
+  # Posts
+  posts = []
+
+  # News API
+  newsapi = News.new(ENV['NEWSAPI_KEY'])
+
+  # News Articles
+  articles = newsapi.get_everything(
+    q: 'crossfit',
+    from: as_of_date,
+    to: as_of_date,
+    language: 'en',
+    sortBy: 'popularity',
+    page: 1,
+    pageSize: 100
+  )
+
+  # Reformat Article Data
+  articles.each do |article|
+    post = {
+      'title' => article.title,
+      'url' => article.url,
+      'image' => article.urlToImage,
+      'source' => article.name,
+      'description' => ''
+    }
+    post['description'] = article.description.gsub(/\r/," ").gsub(/\n/," ") if article.description
+    posts.push(post)
+  end
+
+  # Build Jekyll Front Matter
+  front_matter = {
+    'layout' => 'post',
+    'posts' => posts
+  }
+
+  # Build Jekyll page
   File.open(news_filename, 'w+') do |file|
     file.puts front_matter.to_yaml
     file.puts "---"
   end
+
+else
+  puts "Nothing todo. We've already processed: " + as_of_date + "."
 end
