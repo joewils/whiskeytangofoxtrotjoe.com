c4673b8426a91384f6d0e875d283ab5ee3b31f1e
Joe Wilson <joe@joecode.com>
2019-08-30 12:14:40 -0700

    wtf noise

--- _scripts/wtf.rb
+++ _scripts/wtf.rb
@@ -188,6 +188,11 @@ as_of_dates.each do |as_of_date|
 
 end #as_of_dates.each
 
+
+# 
+username = 'joe%40joecode.com'
+pat = '80a92ed2a91ca972953648414f8c6683792e8c6d'
+#ENV['WTF_GITHUB_USERNAME']
 # Execute Shell Commands to Update GitHub
 puts status = `git status`
 puts "----------"
@@ -195,4 +200,4 @@ puts foobar = `git add -A`
 puts "----------"
 puts commit = `git commit -m "wtf noise"`
 puts "----------"
-puts push = exec('git push https://'+ENV['WTF_GITHUB_USERNAME']+':'+ENV['WTF_GITHUB_PAT']+'@github.com/joewils/whiskeytangofoxtrotjoe.com.git --all')
+puts push = exec('git push https://'+username+':'+pat+'@github.com/joewils/whiskeytangofoxtrotjoe.com.git --all')
